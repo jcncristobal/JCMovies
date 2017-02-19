59b93aecf57204f476967d63fb4c23672f24e4fe
JC <JC@JC-DESKTOP>
2017-02-19 19:26:31 +0800

    Add project files.

---
+++ JCMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JCMovies.Startup))]
+namespace JCMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
